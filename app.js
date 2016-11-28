30dd9b8bb73f6765d0e4b09eb6d0bd664b539e29
Gav Wood <gavin@ethcore.io>
2016-11-28 12:19:31 +0100

    Fix up a lot of minor issues. Persistency.

--- app.js
+++ app.js
@@ -39,20 +39,28 @@ function evaluate(x) {
 function displayReady(x) {
   if (x === undefined)
     return '<span class="undefinedType">undefined</span>';  
+  if (x === null)
+    return '<span class="undefinedType">null</span>';  
   if (typeof(x) == "string")
-    return '"<span class="'+typeof(x)+'Type">' + escapeHtml(x) + '</span>"';
+    return `"<span class="${typeof(x)}Type">${escapeHtml(x)}</span>"`;
   if (Object.prototype.toString.call(x) === '[object Array]')
-    return '[' + x.map(displayReady).join(', ') + ']'
+    return `[${x.map(displayReady).join(', ')}]`;
+  if (typeof(x) == "function")
+    return `<span class="${typeof(x)}Type">function () { /* ... */ }</span>`;
   if (typeof(x) == "object") {
     if (x.toString().indexOf('[object ') != 0)
-      return '<span class="' + x.constructor.name + 'Object">' + escapeHtml(x.toString()) + '</span>';
-    return '<span class="objectType ' + x.constructor.name + 'Object">' + x.constructor.name + ' {' + Object.keys(x).map(f => '<span class="fieldType">' + escapeHtml(f) + '</span>: ' + displayReady(x[f])).join(', ') + "}</span>";
+      return `<span class="${x.constructor.name}Object">${escapeHtml(x.toString())}</span>`;
+    return `<span class="objectType ${x.constructor.name}Object">${x.constructor.name} {${Object.keys(x).map(f => `<span class="fieldType">${escapeHtml(f)}</span>: ${displayReady(x[f])}`).join(', ')}}</span>`;
   }
-  return '<span class="'+typeof(x)+'Type">' + escapeHtml(JSON.stringify(x)) + "</span>";
+  return `<span class="${typeof(x)}Type">${escapeHtml(JSON.stringify(x))}</span>`;
 }
 
-window.historyData = [];
-window.historyIndex = 0;
+if (!localStorage.history)
+  localStorage.history = "[]";
+window.historyData = JSON.parse(localStorage.history);
+window.historyIndex = window.historyData.length;
+if (!localStorage.watches)
+  localStorage.watches = "[]";
 window.watches = [];
 
 function watch(name, f) {
@@ -60,6 +68,14 @@ function watch(name, f) {
   status.innerHTML += '<div class="watch" id="watch' + window.watches.length + '"><span class="expr" id="expr' + window.watches.length + '"">'+escapeHtml(name)+'</span><span class="res" id="res' + window.watches.length + '""></span></div>';
   window.watches.push(f);
 }
+
+if (typeof(window.parity) == 'object')
+  watch('latest', () => window.web3.eth.blockNumber);
+else
+  watch('time', () => new Date);
+
+var savedWatches = JSON.parse(localStorage.watches);
+savedWatches.forEach(w => watch(w[1], () => evaluate(w[0])));
 
 function refreshWatches() {
   window.watches.forEach((x, i) => {
@@ -89,12 +105,17 @@ function exec() {
   if (c != '') {
     command.value = "";
     window.historyData.push(c);
+    while (window.historyData.length > 1000)
+      window.historyData.shift;
+    localStorage.history = JSON.stringify(window.historyData);
     window.historyIndex = window.historyData.length;
 
     var html = '';
     if (c.indexOf("//") != -1) {
       x = c.split("//");
       let e = x[0];
+      savedWatches.push(x);
+      localStorage.watches = JSON.stringify(savedWatches);
       watch(x[1], () => evaluate(e));
       pushLine('<div class="entry command"><span class="type">&gt;</span><span class="text">' + escapeHtml(c) + '</span></div>');
       pushLine('<div class="entry addwatch"><span class="type">✓</span><span class="text">Watch added</span></div>');
@@ -130,52 +151,92 @@ var currentAuto = null;
 var currentPots = [];
 var currentStem = null;
 
-document.getElementById("command").addEventListener("keydown", function(event) {
-  if (event.keyCode == 38 || event.keyCode == 40) {
-    event.preventDefault();
-    if (currentAuto != null) {
-      var i = currentPots.indexOf(currentAuto);
-      document.getElementById('pot' + i).classList = ['ac-unselected'];
-      if (event.keyCode == 40 && i < currentPots.length - 1)
-        ++i;
-      else if (event.keyCode == 38 && i > 0)
-        --i;
-      currentAuto = currentPots[i];
-      let sel = document.getElementById('pot' + i);
-      sel.classList = ['ac-selected'];
-      sel.scrollIntoViewIfNeeded();
-    }
-  } else if (event.keyCode == 39 || event.keyCode == 9 || event.keyCode == 13) {
-    if (currentAuto != null) {
-      document.getElementById("command").value = (currentStem != '' ? currentStem + '.' : '') + currentAuto;
-      document.getElementById("autocomplete").hidden = true;
-      currentAuto = null;
-      event.preventDefault();
-    } 
+function updateAutocomplete() {
+  let v = document.getElementById("command").value;
+  if (v.length == 0) {
+    cancelAutocomplete();
+    return;
+  }
+  let t = v.split('.');
+  let last = t.pop();
+  let tj = t.join('.');
+  let ex = t.length > 0 ? tj : 'window';
+  if (currentStem != tj) {
+    autocompletes = eval('getAllPropertyNames('+ex+')');
+    currentStem = tj;
+  }
+  let dl = document.getElementById("autocomplete");
+  currentPots = autocompletes.filter(n => n.startsWith(last));
+  if (currentPots.length > 0) {
+    if (currentPots.indexOf(currentAuto) == -1)
+      currentAuto = currentPots[0]; 
+    dl.innerHTML = currentPots
+//    .map(n => `${tj != '' ? tj + '.' : ''}${n}`)
+      .map((n, i) => `<div id="pot${i}" class="${currentAuto == n ? 'ac-selected' : 'ac-unselected'}"><span class="ac-already">${escapeHtml(last)}</span><span class="ac-new">${escapeHtml(n.substr(last.length))}</div>`)
+      .join('');
+    dl.hidden = false;
   } else {
+    cancelAutocomplete();
+  }
+}
+
+function enactAutocomplete() {
+  if (currentAuto != null) {
+    document.getElementById("command").value = (currentStem != '' ? currentStem + '.' : '') + currentAuto;
+    cancelAutocomplete();
+  }  
+}
+
+function cancelAutocomplete() {
+  document.getElementById("autocomplete").hidden = true;
+  currentAuto = null;
+}
+
+function scrollAutocomplete(positive) {
+  if (currentAuto != null) {
+    var i = currentPots.indexOf(currentAuto);
+    document.getElementById('pot' + i).classList = ['ac-unselected'];
+    if (positive && i < currentPots.length - 1)
+      ++i;
+    else if (!positive && i > 0)
+      --i;
+    currentAuto = currentPots[i];
+    let sel = document.getElementById('pot' + i);
+    sel.classList = ['ac-selected'];
+    sel.scrollIntoViewIfNeeded();
+  }
+}
+
+document.getElementById("command").addEventListener("onpaste", updateAutocomplete);
+document.getElementById("command").addEventListener("oninput", updateAutocomplete);
+
+document.getElementById("command").addEventListener("keydown", function(event) {
+  let el = document.getElementById("command"); 
+  if (currentAuto != null) {
+    if (event.keyCode == 38 || event.keyCode == 40) {
+      event.preventDefault();
+      scrollAutocomplete(event.keyCode == 40);
+    }
+    else if ((event.keyCode == 39 || event.keyCode == 9 || event.keyCode == 13) && el.selectionStart == el.value.length) {
+      event.preventDefault();
+      enactAutocomplete();
+    }
+    else if (event.keyCode == 27) {
+      event.preventDefault();
+      cancelAutocomplete();
+    }
+  }
+  if (event.keyCode >= 48 || event.keyCode == 8) {
+    let t = document.getElementById("command").value;
     setTimeout(() => {
-      let t = document.getElementById("command").value.split('.');
-      let last = t.pop();
-      let tj = t.join('.');
-      let ex = t.length > 0 ? tj : 'window';
-      if (currentStem != tj) {
-        autocompletes = eval('getAllPropertyNames('+ex+')');
-        currentStem = tj;
-      }
-      let dl = document.getElementById("autocomplete");
-      currentPots = autocompletes.filter(n => n.startsWith(last));
-      if (currentPots.length > 0) {
-        if (currentPots.indexOf(currentAuto) == -1)
-          currentAuto = currentPots[0]; 
-        dl.innerHTML = currentPots
-//          .map(n => `${tj != '' ? tj + '.' : ''}${n}`)
-          .map((n, i) => `<div id="pot${i}" class="${currentAuto == n ? 'ac-selected' : 'ac-unselected'}"><span class="ac-already">${escapeHtml(last)}</span><span class="ac-new">${escapeHtml(n.substr(last.length))}</div>`)
-          .join('');
-        dl.hidden = false;
-      } else {
-        currentAuto = null;
-        dl.hidden = true;
-      }
+      if (t != document.getElementById("command").value)
+        updateAutocomplete();
+    }, 0);
+  }
+  else {
+    setTimeout(() => {
+      if (el.selectionStart != el.value.length)
+        cancelAutocomplete();
     }, 0);
   }
 });
@@ -202,11 +263,9 @@ document.getElementById("command").addEventListener("keyup", function(event) {
 
 if (typeof(window.parity) == 'object') {
   document.getElementById("command").focus();
-  watch('#', () => window.web3.eth.blockNumber);
   window.web3 = web3;
   window.parity = parity;
-} else {
-  watch('time', () => new Date);
 }
+refreshWatches();
 
 ["debug", "error", "info", "log", "warn"].forEach(n => { let old = window.console[n]; window.console[n] = x => { old(x); newLog(n, x); }; });
